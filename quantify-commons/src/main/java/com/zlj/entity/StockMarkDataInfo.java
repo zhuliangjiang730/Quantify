a15359e6649e4128e2c8c32d91aa1354d528e34b
zhuliangjiang730 <sczlj1991@gmail.com>
2020-10-23 17:19:12 +0800

    建立市场行情服务模块以及公共信息模块，并编写行情行情数据对象

---
+++ quantify-commons/src/main/java/com/zlj/entity/StockMarkDataInfo.java
@@ -0,0 +1,21 @@
+package com.zlj.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockMarkDataInfo {
+	private double openPrice; //开盘价
+	private double highPrice; //最高价
+	private double lowPrice; //最低价
+	private double closePrice; //收盘价
+	private double turnoverVol; //成交量
+	private double turnoverAmt; //成交额
+	private double handTurnoveRate; //换手率
+	private double circulationMarketValue; //流通市值
+	private double PERatio; //市盈率
+	private String dataType; //数据类型：1-1分钟数据，2-5分钟数据，3-15分钟数据，4-30分钟数据，5-一小时数据，6-日数据、7-周数据，8-月数据，9-季数据，10-年数据，
+}
